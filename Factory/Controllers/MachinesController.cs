12d6b3c3409181d7ae4c8f32544a65c8990f8dbd
Will W <wjwat@onionslice.org>
2022-03-18 11:51:40 -0500

    Add code for controllers

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Factory.Controllers
+{
+    public class MachinesController : Controller
+    {
+
+      public ActionResult Index()
+      {
+        return View();
+      }
+
+    }
+}
