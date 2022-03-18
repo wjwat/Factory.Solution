5c4a5cf5829bc4ccf59fa4aaedf1b05d27f49c4b
Will W <wjwat@onionslice.org>
2022-03-18 15:54:47 -0500

    Small change to models

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,6 +8,7 @@ namespace Factory.Models
     {
       Authorizations = new HashSet<EngineerMachine> {};
     }
+
     public int MachineId { get; set; }
     public string ModelName { get; set; }
     public virtual ICollection<EngineerMachine> Authorizations { get; }
