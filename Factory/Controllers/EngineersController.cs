ba0f8bf2a53c7049c85de9882abcd5d40dd53ede
Will W <wjwat@onionslice.org>
2022-03-18 18:39:59 -0500

    WIP: Checkboxes may have been the wrong way to go

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -31,8 +31,9 @@ namespace Factory.Controllers
       }
 
       [HttpPost]
-      public ActionResult Create(Engineer engineer)
+      public ActionResult Create(Engineer engineer, int[] MachineId)
       {
+        System.Console.WriteLine(MachineId.Length);
         _db.Engineers.Add(engineer);
         _db.SaveChanges();
         return RedirectToAction("Index");
@@ -50,12 +51,26 @@ namespace Factory.Controllers
       public ActionResult Edit(int id)
       {
         var engineer = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
+        ViewBag.Machines = _db.Machines.ToList();
+        ViewBag.AuthorizedMachines = _db.EngineerMachine
+            .Where(e => e.EngineerId == id)
+            .Select(m => m.MachineId)
+            .ToList();
+        System.Console.WriteLine(ViewBag.AuthorizedMachines.Count);
         return View(engineer);
       }
 
       [HttpPost]
-      public ActionResult Edit(Engineer engineer)
+      public ActionResult Edit(Engineer engineer, int[] MachineId)
       {
+        foreach (int m in MachineId)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() {
+            EngineerId = engineer.EngineerId,
+            MachineId = m
+          });
+        }
+
         _db.Entry(engineer).State = EntityState.Modified;
         _db.SaveChanges();
         return RedirectToAction("Index");
