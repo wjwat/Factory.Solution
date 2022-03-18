06e7fd83f31112f8d55b187f8ae916710ac81bc4
Will W <wjwat@onionslice.org>
2022-03-18 15:54:17 -0500

    Pass in Engineers & Machines

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
 
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
+
       public ActionResult Index()
       {
+        ViewBag.Engineers = _db.Engineers.ToList();
+        ViewBag.Machines = _db.Machines.ToList();
         return View();
       }
 
